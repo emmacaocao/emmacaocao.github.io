e6630a037b51e70be925e95e13f582278d17ba9d
Jiesi Ren <jiesiiren@gmail.com>
2016-02-27 14:13:19 -0500

    Refactor site to an actual legit site. Add jquery transit, parallax lib, and update layouts and assets.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,4 @@
+var LOG = true;
 
 var UNIVERSE = UNIVERSE || {};
 
@@ -7,23 +8,113 @@ var UNIVERSE = UNIVERSE || {};
 
   UNIVERSE.initialize = {
     init: function() {
+      UNIVERSE.initialize.fullScreen();
+      UNIVERSE.initialize.parallax();
+    },
 
-      //UNIVERSE.initialize.stuff1();
-    }
+    fullScreen: function() {
+			if ( $fullScreenEl.length > 0 ) {
+				$fullScreenEl.each( function() {
+					var element = $(this),
+						scrHeight = $window.height();
+
+          // Get parent full-screen height
+					if ( element.parents('.full-screen').length > 0 ) {
+            scrHeight = element.parents('.full-screen').height();
+          }
+
+					element.css('height', scrHeight);
+				});
+			}
+		},
+
+    parallax: function() {
+      $('#splash').parallax({imageSrc: './assets/img/city_background.jpg'});
+    },
+
+    /*
+    // Custom parallax not used, replaced by init.parallax() right now
+    windowScroll: function() {
+      window.addEventListener('scroll', function(){
+        requestAnimationFrame(function(){
+          UNIVERSE.slider.sliderParallax();
+          // UNIVERSE.slider.sliderElementsFade();
+        });
+      }, false);
+    }*/
   };
 
   UNIVERSE.header = {};
 
-  UNIVERSE.slider = {};
+  UNIVERSE.slider = {
 
-  UNIVERSE.documentOnResize = {};
+    /*
+    // Custom parallax not used, replaced by init.parallax() right now
+    // Parallax offset setter called every frame on scroll
+    sliderParallax: function() {
+  		if( $sliderParallaxEl.length > 0 ) {
+        if (!UNIVERSE.isMobile.any()) {
+  				var parallaxElHeight = $sliderParallaxEl.outerHeight();
 
-  UNIVERSE.documentOnReady = {};
+          // After 50 pixels from top, begin parallax translation
+  				if( ( parallaxElHeight + 50 ) > $window.scrollTop() ) {
+  					if ($window.scrollTop() > 0) { // Not at top
+              var parallaxDivisor = 1.5; // 1 = fixed -> numerator = moves completely with scroll
+  						var tranformAmount = ($window.scrollTop() / parallaxDivisor ).toFixed(2);
+  						$sliderParallaxEl.stop(true, true).transition({ y: tranformAmount }, 0);
+  					} else {
+  						$sliderParallaxEl.transition({ y: 0 }, 0); // At top
+  					}
+  				}
+        } else {
+  				$sliderParallaxEl.transition({ y: 0 }, 0); // No parallax elements
+  			}
+  		}
+  	}*/
+
+  };
+
+  UNIVERSE.isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function() {
+			return (UNIVERSE.isMobile.Android() || UNIVERSE.isMobile.BlackBerry() ||
+              UNIVERSE.isMobile.iOS() || UNIVERSE.isMobile.Opera() ||
+              UNIVERSE.isMobile.Windows());
+		}
+	};
+
+  UNIVERSE.documentOnResize = {
+    init: function() {
+      UNIVERSE.initialize.fullScreen(); // Fix full screen elements on resize
+    }
+  };
+
+  UNIVERSE.documentOnReady = {
+    init: function() {
+      if (LOG) console.log("documentOnReady.init();");
+      UNIVERSE.initialize.init();
+    }
+  };
 
   UNIVERSE.documentOnLoad = {};
 
   var $window = $(window),
-    $body = $('body');
+    $body = $('body'),
+    $fullScreenEl = $('.full-screen');
 
   $(document).ready( UNIVERSE.documentOnReady.init );
   $window.load( UNIVERSE.documentOnLoad.init );
